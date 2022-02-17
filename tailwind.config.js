156f464461c04d66cdd3178921d2b845c86fdb65
Germain Bo <gbld@mailo.com>
2022-02-17 12:10:30 +0100

    tailwind config font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'redHat': 'Red Hat Display',
+        'shizuru': 'Shizuru'
+      },
+      colors:{
+        myBlue: {
+          50: '#f5f7ff',
+          150: '#e0e8ff',
+          650: '#7280a7',
+          750: '#3829e0',
+          850: '#1f2f56'
+        }
+      }
+    },
   },
   plugins: [],
 }
